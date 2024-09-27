1802d5655a2d3d1d358049b5c0d872e1ed5290fd
23DP4SSavi <166484686+23DP4SSavi@users.noreply.github.com>
2024-09-27 09:54:23 +0000

    Speeding Ticket

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,11 +1,19 @@
 package lv.rvt;
 
-public class App 
+import java.util.*;
+
+public class App
 {
     public static void main( String[] args )
     {
-        // My code starts here
-        System.out.println("Hello World!");
-        // Code ends
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give speed:");
+        double number = Double.valueOf(scanner.nextLine());
+        if (number > 120) {
+            System.out.println("Speeding ticket!");
+        }
+    
+    
+    
     }
 }
