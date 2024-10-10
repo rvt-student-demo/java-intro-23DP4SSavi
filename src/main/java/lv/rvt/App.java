ff2a4b7b6a3a83745633bafb24cac318f99f074e
23DP4SSavi <166484686+23DP4SSavi@users.noreply.github.com>
2024-10-10 11:07:31 +0000

    fix devcontainer

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,18 +10,18 @@ public class App
 
         int number = 5;
 
-if (number % 5 == 0) {
-    System.out.println("The number is divisible by five!");
-}
+        if (number % 5 == 0) {
+            System.out.println("The number is divisible by five!");
+        }
 
-if (number % 6 != 0) {
-    System.out.println("The number is not divisible by six!");
-}
-Scanner reader = new Scanner(System.in);
-int number = Integer.valueOf(reader.nextLine());
+        if (number % 6 != 0) {
+            System.out.println("The number is not divisible by six!");
+        }
+        Scanner reader = new Scanner(System.in);
+        int number = Integer.valueOf(reader.nextLine());
 
-if (number % 4!= 0) {
-    System.out.println("The year is not a leap year.");
+        if (number % 4!= 0) {
+            System.out.println("The year is not a leap year.");
  
     }
 
