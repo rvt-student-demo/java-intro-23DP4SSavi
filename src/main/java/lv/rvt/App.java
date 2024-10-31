3fff18c38d17c1ba8c2b3876d5123f5283f10b09
23DP4SSavi <166484686+23DP4SSavi@users.noreply.github.com>
2024-10-31 12:51:04 +0000

    arraySum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,22 +7,19 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
-        int sum = 0; 
-        System.out.println("Write numbers: ");
+        {
+            int[] val = {0, 1, 2, 3}; 
+        
+            int sum = 0;
+            for(int i = 0; i < val.length; i++) {
+                sum += val[i];
+            }   
+            System.out.println( "Sum of all numbers = " + sum );
+         
+           }
+        
 
-        while (true) {
-            int number = scanner.nextInt();
-            if (number == -1) {
-                break; 
-            }
-            sum += number; 
-        }
-
-        System.out.println("Thx! Bye!");
-        System.out.println("Sum of the numbers: " + sum);
         scanner.close();
     }
+
 }
-
-
-
