0867c525d2562eb89be6cda48e990572521a145b
23DP4SSavi <166484686+23DP4SSavi@users.noreply.github.com>
2024-11-01 10:57:14 +0000

    reverseOrder

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,25 +7,17 @@ public class App
     public static void main( String[] args ) {}
     }
         
-    class Exercise4
+    class Exercise5
     {
       public static void main ( String[] args )
       {
-        int[] valA   = { 13, -22,  82,  17}; 
-        int[] valB   = {  0,   0,   0,   0};
+        int[] val = {0, 1, 2, 3}; 
         
-        for(int i = 0; i < 4; i++){
-            valB[i] = 25 - valA[i];
-        }
      
-        System.out.println( "valA: " 
-            + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+        System.out.println( "Original Array: " 
+            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
      
-        System.out.println( "valB: " 
-            + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
     
-        System.out.println( "sum:  " 
-            + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
-            + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
+        System.out.println( "Reversed Array: " + val[3] + " " + val[2] + " " + val[1] + " " + val[0] );
        }
     }
