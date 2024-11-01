c861f436e1795253458ea75d7454c576b518d91d
23DP4SSavi <166484686+23DP4SSavi@users.noreply.github.com>
2024-11-01 10:49:41 +0000

    sameSum

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,21 +7,25 @@ public class App
     public static void main( String[] args ) {}
     }
         
-    class Exercise3
+    class Exercise4
     {
       public static void main ( String[] args )
       {
         int[] valA   = { 13, -22,  82,  17}; 
-        int[] valB   = {-12,  24, -79, -13};
-        int[] sum    = {  0,   0,   0,   0};
-         
-        for (int i=0;i < 4; i++){
-            sum[i] = valA[i] + valB[i];
- 
-    
-    
+        int[] valB   = {  0,   0,   0,   0};
+        
+        for(int i = 0; i < 4; i++){
+            valB[i] = 25 - valA[i];
         }
-        System.out.println( "sum: " 
-            + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+     
+        System.out.println( "valA: " 
+            + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+     
+        System.out.println( "valB: " 
+            + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+    
+        System.out.println( "sum:  " 
+            + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
+            + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
        }
     }
