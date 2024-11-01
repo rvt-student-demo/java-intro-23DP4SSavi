2cf30b9f3869886ad7e1cc48fbe15d005a342807
23DP4SSavi <166484686+23DP4SSavi@users.noreply.github.com>
2024-11-01 10:45:47 +0000

    threeArrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,23 +3,25 @@ package lv.rvt;
 
 
 public class App
-{
-    public static void main( String[] args )
-{
-        
-        {
-            int[] val = {13, -4, 82, 17}; 
-            int[] twice = new int[4];
-
-            System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-
-            twice[0] = val[0] * 2;
-            twice[1] = val[1] * 2;
-            twice[2] = val[2] * 2;
-            twice[3] = val[3] * 2;
-
-        
-            System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
-        }
+    {
+    public static void main( String[] args ) {}
     }
-}
+        
+    class Exercise3
+    {
+      public static void main ( String[] args )
+      {
+        int[] valA   = { 13, -22,  82,  17}; 
+        int[] valB   = {-12,  24, -79, -13};
+        int[] sum    = {  0,   0,   0,   0};
+         
+        for (int i=0;i < 4; i++){
+            sum[i] = valA[i] + valB[i];
+ 
+    
+    
+        }
+        System.out.println( "sum: " 
+            + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+       }
+    }
