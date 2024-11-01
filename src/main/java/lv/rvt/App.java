f58002030b3c8ea811130a4b2f96f83118219f9f
23DP4SSavi <166484686+23DP4SSavi@users.noreply.github.com>
2024-11-01 10:37:16 +0000

    twoArrays

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -1,28 +1,25 @@
 package lv.rvt;
 
-import java.util.*;
+
 
 public class App
 {
     public static void main( String[] args )
-    {
-        Scanner scanner = new Scanner(System.in);
+{
+        
         {
             int[] val = {13, -4, 82, 17}; 
-            int[] twice = new int[val.length];
+            int[] twice = new int[4];
 
-            for (int i = 0; i < val.length; i++) {
-                
-            }
-            
-            System.out.println(
-                Arrays.toString(twice)
-            );
-            System.out.println( "Original Array: " 
-                + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+            System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+
+            twice[0] = val[0] * 2;
+            twice[1] = val[1] * 2;
+            twice[2] = val[2] * 2;
+            twice[3] = val[3] * 2;
+
         
-            System.out.println( "New Array: " 
-                + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
-           }
+            System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
         }
     }
+}
