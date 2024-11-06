9aea12f7f0aad82c4cfd279c55bd2fd9568f6082
23DP4SSavi <166484686+23DP4SSavi@users.noreply.github.com>
2024-11-06 13:17:43 +0000

    Account

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -6,18 +6,4 @@ public class App
     {
     public static void main( String[] args ) {}
     }
-        
-    class Exercise5
-    {
-      public static void main ( String[] args )
-      {
-        int[] val = {0, 1, 2, 3}; 
-        
-     
-        System.out.println( "Original Array: " 
-            + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
-     
-    
-        System.out.println( "Reversed Array: " + val[3] + " " + val[2] + " " + val[1] + " " + val[0] );
-       }
-    }
+
